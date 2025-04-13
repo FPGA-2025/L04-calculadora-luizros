cc0bd172cc736b64b0254ad1a9ace7b82c2de304
Luiz <luizrodriguesesilva@outlook.com.br>
2025-04-13 12:10:31 -0300

    exercicio 04

--- calculadora.v
+++ calculadora.v
@@ -5,6 +5,14 @@ module calculadora(
   output  [7:0] saida
 );
 
-// insira seu código aqui
+  wire [7:0] soma     = entrada_A + entrada_B;
+  wire [7:0] subtracao = entrada_A - entrada_B;
+
+  assign saida =
+    (codigo == 3'b001) ? entrada_A :
+    (codigo == 3'b010) ? entrada_B :
+    (codigo == 3'b011) ? soma      :
+    (codigo == 3'b100) ? subtracao :
+                         8'b00000000; 
 
 endmodule
